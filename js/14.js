58b8d58d40096ac48bd266ea89ea1111ed02fa58
s.fedyanin <s.fedyanin@skbkontur.ru>
2024-12-14 12:36:22 +0500

    Remove unused func at Day 14

--- js/14.js
+++ js/14.js
@@ -130,7 +130,3 @@ function countRobotQuadrants(width, height, robots) {
 
 	return robotQuadrants
 }
-
-function replaceAt(str, index, replacement) {
-    return str.substring(0, index) + replacement + str.substring(index + replacement.length);
-}
